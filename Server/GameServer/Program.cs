38854641a9f970c72855cf06c78d9c98bf43f374
artemkoncevoj4 <artemkoncevoj4@gmail.com>
2026-01-07 04:28:21 +0300

    server backend v1

--- Server/GameServer/Program.cs
+++ Server/GameServer/Program.cs
@@ -1,12 +1,13 @@
 using Microsoft.EntityFrameworkCore;
 using GameServer.Database;
-
+using Microsoft.EntityFrameworkCore.Diagnostics; // Не забудь этот using!
 var builder = WebApplication.CreateBuilder(args);
 
 // 1. Настройка базы данных PostgreSQL
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(connectionString));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+           .ConfigureWarnings(w => w.Ignore(RelationalEventId.PendingModelChangesWarning)));
 
 // 2. Регистрация нашего хешера
 builder.Services.AddScoped<IPasswordHasher, BCryptHasher>();
